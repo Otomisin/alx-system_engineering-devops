68292ac1648758bb9b74a8ce6e75d0850df580d7
otomisin <orenaiket@yahoo.com>
2022-12-06 07:02:29 +0100

    noon

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a function that replaces an element in a list at a specific position without modifying the original list (like in C).
+Write a function that replaces an element in a list at a specific 
+position without modifying the original list (like in C).
 """
 def new_in_list(my_list, idx, element):
     copied_list = my_list.copy()
