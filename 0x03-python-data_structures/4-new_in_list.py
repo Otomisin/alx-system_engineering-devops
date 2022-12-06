4ceac8a2afa22b053bb1c18e0c4f7af9a8bb91e5
otomisin <orenaiket@yahoo.com>
2022-12-06 07:04:22 +0100

    noon

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-
-"""
-Write a function that replaces an element in a list at a specific 
-position without modifying the original list (like in C).
-"""
 def new_in_list(my_list, idx, element):
-    copied_list = my_list.copy()
+    copy = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
-        return copied_list
+        return my_list.copy()
     else:
-        copied_list[idx] = element
-        return copied_list
-
-"""
-def new_in_list(my_list, idx, element):
-    newList = [None] * len(my_list)
-    for i in range(0, len(my_list)):
-        if i == idx:
-            newList[i] = element
-        else:
-            newList[i] = my_list[i]
-    return newList
-"""
+        copy[idx] = element
+        return copy
 
