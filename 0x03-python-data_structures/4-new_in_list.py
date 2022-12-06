8823122b4ad210b274b8c1add994a92a43552283
otomisin <orenaiket@yahoo.com>
2022-12-06 08:00:15 +0100

    noonday6

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
 
+"""
+Write a function that replaces an element in a list at a specific 
+position without modifying the original list (like in C).
+"""
+
+def new_in_list(my_list, idx, element):
 
     copied_list = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,20 +2,6 @@
 """
 Write a function that prints a matrix of integers.
 """
-"""
-def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        a = ""
-        for j in range(0, len(matrix[i])):
-            if matrix[i][j] in range(0, 1000000):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j < len(matrix[i]) - 1:
-                     print(" ", end="")
-        print()
-
-
-"""
-
 def print_matrix_integer(matrix=[[]]):
     for each_list in matrix:
         for i in each_list:
