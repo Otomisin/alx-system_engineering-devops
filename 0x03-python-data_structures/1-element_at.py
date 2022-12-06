38c1ec068e6375b12c919768421e8c6100ffa7a0
otomisin <orenaiket@yahoo.com>
2022-12-06 08:44:25 +0100

    noonday7

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,7 @@ Write a function that retrieves an element from a list like in C
 """
 # that retrieves an element from a list like in C.
 
+
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,10 @@ Write a function that replaces an element in a list at a specific
 position without modifying the original list (like in C).
 """
 # pylint: disable = invalid-name
+# pylint: disable = missing-function-docstring
+# pylint: disable = consider-using-enumerate
+
+
 def new_in_list(my_list, idx, element):
     newList = [None] * len(my_list)
     for i in range(0, len(my_list)):
