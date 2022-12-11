6a9d8fb763526802d67aa1b4312758e3135b52a1
otomisin <orenaiket@yahoo.com>
2022-12-11 06:44:59 +0100

    new

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+"""
+Complete this source code in order to print the integer stored in the variable number, 
+followed by Battery street, followed by a new line.
+
+"""
 number = 98
 print(f"{number:d} Battery street")
