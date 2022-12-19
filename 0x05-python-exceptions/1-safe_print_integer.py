cf80f89dbce15c9a742c098eca565ca3034dc677
Otomisin <orenaiket@yahoo.com>
2022-12-19 17:54:28 +0000

    all7

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -15,30 +15,3 @@ def safe_print_integer(value):
         return (True)
     except (TypeError, ValueError):
         return (False)
-
-
-
-# """
-# Write a function that prints the first x elements of a list and only integers.
-# """
-# """
-# TEST CODE
-# """
-
-# # Test 1
-# value = 89
-# has_been_print = safe_print_integer(value)
-# if not has_been_print:
-#     print("{} is not an integer".format(value))
-
-# # Test 2
-# value = -89
-# has_been_print = safe_print_integer(value)
-# if not has_been_print:
-#     print("{} is not an integer".format(value))
-
-# # Test 3
-# value = "School"
-# has_been_print = safe_print_integer(value)
-# if not has_been_print:
-#     print("{} is not an integer".format(value))
