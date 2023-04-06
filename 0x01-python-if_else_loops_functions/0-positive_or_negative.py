d203e0c0d844197f59d68362f6ffb72cab876a09
Oluwatosin Orenaike <orenaiket@yahoo.com>
2023-04-06 05:17:57 +0100

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This program will assign a random signed number to the variable number each time it is executed
+This program will assign a random signed number to the variable number each time it is executed. 
 """
 import random
 number = random.randint(-10, 10)
