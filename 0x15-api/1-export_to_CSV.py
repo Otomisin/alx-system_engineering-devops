d3b205000760aa4cd51b96f68f48c378b55c24ad
otomisin <orenaiket@yahoo.com>
2023-04-24 19:36:23 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """Exports to-do list information for a given employee ID to CSV format."""
 import csv
-import json
+import requests
 import sys
-from urllib import request, parse
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = json.loads(request.urlopen(f"{url}users/{user_id}").read())
-    username = user.get("username")
-    user_todos = json.loads(
-        request.urlopen(f"{url}todos?
-                        {parse.urlencode({'userId': user_id})}").read())
+    user_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(user_url + "users/{}".format(user_id)).json()
+    user_name = user.get("username")
+    user_todos = requests.get(
+        user_url + "todos", params={"userId": user_id}).json()
 
-    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for todo in user_todos:
-            writer.writerow([user_id,
-                             username, todo.get("completed"),
-                             todo.get("title")])
+        [writer.writerow(
+            [user_id, user_name, t.get("completed"), t.get("title")]
+         ) for t in user_todos]
