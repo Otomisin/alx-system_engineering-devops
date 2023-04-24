2a6fd645b69ce963b920e9c9d3478da8cb90ddc2
otomisin <orenaiket@yahoo.com>
2023-04-24 18:41:37 +0100

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user_todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
-            } for t in todos]}, jsonfile)
+            } for t in user_todos]}, jsonfile)
